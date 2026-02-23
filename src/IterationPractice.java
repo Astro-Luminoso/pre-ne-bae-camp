6812ac383c7fec14a3a26e677f5d4961075076d7
HanByeol Yang <beuri97@naver.com>
2026-02-23 16:03:44 +0900

    feat: created new practice class 'IterationPractice.java'

---
+++ src/IterationPractice.java
@@ -0,0 +1,38 @@
+import java.util.stream.IntStream;
+
+public class IterationPractice {
+    public static void main(String[] args) {
+
+        int index = 0;
+        while (index < 100) {
+            System.out.println(++index);
+        }
+
+        for(int i=1; i<=100; i++) {
+            System.out.println(i);
+        }
+
+        int evenIndex = 0;
+        do {
+            evenIndex += 2;
+            System.out.println(evenIndex);
+        } while (evenIndex<100);
+        for(int i=2; i<=100; i+=2) {
+            System.out.println(i);
+        }
+
+        for(int i=2; i<=9; i++){
+            for(int j=1; j<=9; j++) {
+                System.out.printf("%d ", i*j);
+            }
+            System.out.println();
+        }
+
+        IntStream.rangeClosed(2, 9).forEach(i -> {
+            IntStream.rangeClosed(1, 9).forEach(j -> {
+                System.out.printf("%d ", i*j);
+            });
+            System.out.println();
+        });
+    }
+}
