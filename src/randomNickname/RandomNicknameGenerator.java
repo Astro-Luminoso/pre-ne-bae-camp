b6264355e3a18aa877fbdfc924c547448611c304
HanByeol Yang <beuri97@naver.com>
2026-02-26 15:27:39 +0900

    refactor: modified RandomNicknameGenerator shorter

--- src/randomNickname/RandomNicknameGenerator.java
+++ src/randomNickname/RandomNicknameGenerator.java
@@ -4,24 +4,35 @@ import java.util.Random;
 
 public class RandomNicknameGenerator {
 
-    private String[] firstAdjectives = {"기철초풍", "멋있는", "재미있는"};
-    private String[] secondAdjectives = {"도전적인", "노란색의", "바보같은"};
-    private String[] nouns = {"돌고래", "개발자", "오랑우탄"};
+    private final String[] firstAdjectives = {"기철초풍", "멋있는", "재미있는"};
+    private final String[] secondAdjectives = {"도전적인", "노란색의", "바보같은"};
+    private final String[] nouns = {"돌고래", "개발자", "오랑우탄"};
 
-    private Random random = new Random();
+    private final Random random = new Random();
 
+    /**
+     * Generates a complete random nickname by combining random adjectives and a noun
+     * @return a randomly generated nickname string
+     */
+    public String generateNickname() {
+        return getRandomElement(firstAdjectives) + " " +
+               getRandomElement(secondAdjectives) + " " +
+               getRandomElement(nouns);
+    }
 
-    public String getRandomNickName(String[] nickNames, int arrayLength) {
-        return nickNames[random.nextInt(arrayLength)];
+    /**
+     * Returns a random element from the given array
+     * @param array the array to select from
+     * @return a randomly selected element
+     */
+    private String getRandomElement(String[] array) {
+        return array[random.nextInt(array.length)];
     }
 
     public static void main(String[] args) {
         RandomNicknameGenerator nicknameGenerator = new RandomNicknameGenerator();
 
         System.out.println("===랜덤 닉네임 생성기===");
-        System.out.printf("%s %s %s%n",
-                nicknameGenerator.getRandomNickName(nicknameGenerator.firstAdjectives, nicknameGenerator.firstAdjectives.length),
-                nicknameGenerator.getRandomNickName(nicknameGenerator.secondAdjectives, nicknameGenerator.secondAdjectives.length),
-                nicknameGenerator.getRandomNickName(nicknameGenerator.nouns, nicknameGenerator.nouns.length));
+        System.out.println(nicknameGenerator.generateNickname());
     }
 }
